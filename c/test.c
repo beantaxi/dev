bc1918ca5d0a19415b40a6607e13832d6e04d98a
Bean Taxi <beantaxi@gmail.com>
2016-02-08 01:16:55 -0600

    socketPoller!

--- c/test.c
+++ c/test.c
@@ -1,7 +1,11 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/epoll.h>
+#include "socketLib.h"
 
-int main (int argc, char* args[])
+
+void testNewStringFunctions ()
 {
 	const char* line = "HTTP/1.1 200 OK";
 	const char* token = " ";
@@ -32,5 +36,54 @@ int main (int argc, char* args[])
 	s += n+1;
 
 	printf("\n");
+}
+
+int stringToFile (const char *s, const char* path)
+{
+	FILE* outfile = fopen(path, "w");
+	fwrite(s, strlen(s), 1, outfile);
+	fclose(outfile);
+	int fdIn = open(path, O_RDONLY);
+	return fdIn;
+}
+
+void testReadUntil ()
+{
+	const char* s;
+	const char* delim;
+
+	s= "HTTP/1.1 200 OK\r\n";
+	in = stringToFile(s, "test.txt");
+	
+	delim = " ";
+	puts(readUntil(in, delim));
+	puts(readUntil(in, delim));
+
+	delim = "\r\n";
+	puts(readUntil(in, delim));
+
+	const char* lastThingee = readUntil(in, delim);
+	if (lastThingee == NULL)
+	{
+		puts("lastThingee is null");
+	}
+	else
+	{
+		puts(lastThingee);
+	}
+}
+
+
+void checkEPollRdHupValue ()
+{
+	printf("EPOLLRDHUP=%d\n", EPOLLRDHUP);
+}
+
+int main (int argc, char* args[])
+{
+	testReadUntil();
+
 	return 0;
 }
+
+
