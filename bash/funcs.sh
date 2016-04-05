413381d10fe5db72cf395bde49d87faea9a5b765
Bean Taxi <beantaxi@gmail.com>
2016-04-05 17:54:23 -0500

    cleanup on funcs.sh

--- bash/funcs.sh
+++ bash/funcs.sh
@@ -19,15 +19,9 @@ checkNoVim ()
 	local optionString='n'
 	local args=$(getopt -o $optionString --long novim -n "$BASH_SOURCE" -- "$@")
 	eval set -- "$args"
-	echo "\$1=$1"
 	if [ $1 = '-n' ] || [ $1 = '--novim' ]; then
 		return 1
 	else
 		return 0
 	fi
 }
-
-export -f newScript
-export -f checkNoVim
-
-# checkNoVim "$@"
