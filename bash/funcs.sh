f6f941e076304ca3e0fbd9f7d5ca3e5796813b0a
Bean Taxi <beantaxi@gmail.com>
2016-04-05 17:51:35 -0500

    Added cmdline option to newScript. My first!!!

--- bash/funcs.sh
+++ bash/funcs.sh
@@ -2,8 +2,32 @@
 
 newScript ()
 {
-	touch $1
-	chmod 744 $1
-	echo -e '#!/bin/bash\n\n' > $1
-	vi + -c'startinsert' $1
+	local filename="$1"
+	touch $filename
+	chmod 744 $filename
+	echo -e '#!/bin/bash\n\n' > $filename
+
+	checkNoVim "$@"
+	if [ $? -eq 0 ]; then
+		vi + -c'startinsert' $filename
+	fi
+	
 }
+
+checkNoVim ()
+{
+	local optionString='n'
+	local args=$(getopt -o $optionString --long novim -n "$BASH_SOURCE" -- "$@")
+	eval set -- "$args"
+	echo "\$1=$1"
+	if [ $1 = '-n' ] || [ $1 = '--novim' ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+export -f newScript
+export -f checkNoVim
+
+# checkNoVim "$@"
