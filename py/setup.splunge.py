c2228ffa3f0b495b736795008d0cc8aea99b2635
Chris Lalos <beantaxi@gmail.com>
2016-06-02 15:04:50 -0400

    moved splunge setup file

--- py/setup.splunge.py
+++
@@ -1,7 +0,0 @@
-import setuptools
-
-setuptools.setup(name='splunge',
-                 version='0.1',
-                 url='https://github.com/beantaxi/dev/tree/master/py/splunge',
-                 install_requires=['gunicorn', 'jinja2'],
-                 packages=['splunge'])
